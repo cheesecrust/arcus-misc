442c20fa406a648fe0bfc229247922bfbec1daf8
cheesecrust <jack0810@kookmin.ac.kr>
2024-10-24 09:41:59 +0900

    INTERNAL: fix pom.xml for run with .jar

--- acp-java/src/main/java/jam2in/arcus/acp.java
+++ acp-java/src/main/java/jam2in/arcus/acp.java
@@ -520,6 +520,7 @@ public class acp {
 
   public static void main(String[] args) throws Exception {
     config conf = new config();
+    System.out.println(args[0]);
     String config_path = null;
     for (int i = 0; i < args.length; i++) {
 	    if (args[i].equals("-config")) {
